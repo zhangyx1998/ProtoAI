09b5b2a145724ee16f02517fbf6b0fa7abb640b9
Yuxuan Zhang <github@z-yx.cc>
2025-09-27 15:46:55 -0400

    update core type definitions

--- core/index.d.ts
+++ core/index.d.ts
@@ -6,6 +6,35 @@
 
 type Awaitable<T> = T | Promise<T>;
 
+type Packet = {
+    // Raw packet info
+    type: "DATA-UP" | "DATA-DOWN";
+    timestamp: BigInt;
+    payload: ArrayBuffer;
+    // AI Inferred Properties
+    inferred?: InferredPacketProperties;
+};
+
+type UserHint = {
+    type: "USER-HINT";
+    timestamp: BigInt;
+    payload: string;
+};
+
+type InferredPacketProperties = {
+    readonly protocol_name: string;
+    readonly description: string; // Describes the packet's role or purpose within the protocol
+    readonly confidence: number; // Confidence level of the inference (0 to 1)
+    readonly fields: BinaryField[];
+};
+
+type BinaryField = {
+    readonly name: string; // Name of the field
+    readonly description: string; // Description of the field's purpose
+    readonly start_bit: number; // Start position of the field in bits
+    readonly length: number; // Length of the field in bits
+};
+
 declare module "core" {
     /** Path to the resolved native module */
     export const __origin__: string;
@@ -22,5 +51,18 @@ declare module "core" {
     export class Counter extends CoreObject {
         [Symbol.iterator](): Iterator<number>;
         get value(): number;
+    }
+
+    export class PseudoTTY extends CoreObject {
+        /**
+         * @param tty path to the actual tty serial port of a physical device
+         */
+        static create(tty: string): PseudoTTY;
+        // Path to the emulated TTY device
+        get path(): string;
+        // Connection state change subscriber
+        onConnectionStateChange(callback: (connected: boolean) => any): void;
+        // Data packet subscriber
+        onData(callback: (data: Packet) => any): void;
     }
 }
